293a1c5cf19c79736c1e08f77f85ab1fc2d8f9db
Kichiyaki <xyztojajestem@gmail.com>
2021-08-04 18:13:03 +0200

    update some packages, fix one error caused by next 11

--- src/utils/resolveAs.ts
+++ src/utils/resolveAs.ts
@@ -1,6 +1,7 @@
+import Router from 'next/router'
 import { resolveHref } from 'next/dist/next-server/lib/router/router';
 import { UrlObject } from 'url';
 
-const resolveAs = (url: UrlObject): string => resolveHref('', url, true)[1];
+const resolveAs = (url: UrlObject): string => resolveHref(Router, url, true)[1];
 
 export default resolveAs;
